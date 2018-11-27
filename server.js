9ab0495781c30e8c4041fcdf975097ca8a1221aa
Ronaldo do Carmo <ronaldokarmo@gmail.com>
2018-11-27 16:40:53 -0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) =>{
     res.status(200).send({ message: 'Olá mundo, estou funcionando!'})
 });
 
+app.get('/novarota', (req, res) =>{
+    res.status(200).send({ message: 'Nova rota!'})
+});
+
 app.listen(3000, () => {
     console.log('Api executando na porta 3000');
 })
